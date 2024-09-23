141954ac72258404e50ee5a877fc2ba7759c8571
lebohang_mdm <lebohangmdm@gmail.com>
2024-09-23 18:12:46 +0200

    create a Register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,171 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import logo from "@/public/logo.png";
+import Logo from "@/components/Logo";
+import { RegisterData } from "@/utils/types";
+import { useRouter } from "next/navigation";
+
+// Schema definition
+
+const formSchema = z
+  .object({
+    fullName: z.string().min(2, {
+      message: "Full name must be at least 2 characters.",
+    }),
+    email: z.string().email({
+      message: "Please enter a valid email address.",
+    }),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    passwordConfirm: z.string().min(6, {
+      message: "Confirm password must be at least 6 characters.",
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Passwords do not match.",
+    path: ["passwordConfirm"], // Error will appear under "confirmPassword" field
+  });
+
+const Page = () => {
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  const onSubmit = async (data: RegisterData) => {
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+    }
+
+    return router.push("/login");
+  };
+
+  return (
+    <section className="min-h-screen flex items-center justify-center ">
+      <Card className="w-[350px]">
+        <CardHeader className="mx-auto space-y-6">
+          <div className="flex flex-col items-center gap-y-0.5 relative ">
+            <Image
+              src={logo}
+              alt="logo"
+              className="h-7 w-7 mx-auto object-cover rounded-full  cursor-pointer"
+            />
+            <span className="text-xs sm:text-sm lg:text-base italic absolute top-5 ">
+              seeI<span className="text-red-500">see</span>
+            </span>
+          </div>
+          <CardTitle className="text-center">Register</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="John Doe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="example@mail.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="••••••••"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="passwordConfirm"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="••••••••"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
+  );
+};
+
+export default Page;
